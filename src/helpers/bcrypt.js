a14272d7ffa2a86594456698a263b86751b24694
Gonzalo <gonzalo.rodriguez677@gmail.com>
2025-09-25 09:46:23 -0300

    Se agregó comentarios descriptivos en helpers

--- src/helpers/bcrypt.js
+++ src/helpers/bcrypt.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 
+//Haseo y comparación de contraseñas
 export const hashPassword = async (password) => {
     const saltRounds = 10;
     return await bcrypt.hash(password, saltRounds);
--- src/helpers/jwt.js
+++ src/helpers/jwt.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 
+//Genera y verifica tokens JWT
 export const generateToken = (payload) => {
     try {
         return jwt.sign(payload, process.env.JWT_SECRET, {
@@ -10,6 +11,7 @@ export const generateToken = (payload) => {
     }
 };
 
+//Verifica el token JWT
 export const verifyToken = (token) => {
     try {
         return jwt.verify(token, process.env.JWT_SECRET);
